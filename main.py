05d646d0d993bcc252c1bcc305071b2ac92bdf17
MichaelW <75797622+MagicDash91@users.noreply.github.com>
2025-03-24 22:01:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="127.0.0.1",
-        port=8000,
+        port=9000,
         timeout_keep_alive=600,
         log_level="info",
         access_log=True,
